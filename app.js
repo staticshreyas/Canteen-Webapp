a3bbea6168135fb573d6ff809837a5ab31012a56
Shreyas <shreyas.mm@somaiya.edu>
2020-05-21 00:44:59 +0530

    2

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const validator= require('express-validator');
 var MongoStore= require('connect-mongo')(session);
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const nodemailer = require("nodemailer");
+
 
 var indexRouter = require('./routes/index');
 var userRoutes = require('./routes/user');
